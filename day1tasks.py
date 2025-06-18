b1440074a39547fb793c7bae0219b7477847ff5a
niveshaveludhas30 <niveshaveludhas@gmail.com>
2025-06-18 23:36:24 +0530

    Add files via upload

---
+++ day1tasks.py
@@ -0,0 +1,79 @@
+#task 1
+print("Hello,World!")
+
+#task 2
+print("Nivesha")
+
+#task 3
+print("Krish")
+print(5)
+
+#task 4
+emotion="happy"
+person="aishu"
+odd=1
+even=2
+price=25.94
+rain=False
+print(emotion+person)
+print(odd*even)
+print("offer price is:",price/2)
+print("Is it hot:",not rain)
+
+#task 5
+name="krish"
+age="5"
+message="I am " + name + " and I am " + age + " years old"
+print(message)
+
+#task 6
+employee="krish"
+age=40
+print("Name of the employee is ",employee)
+print("Age of the employee is ",age)
+
+#task 7
+name1="aishu"
+name2="abi"
+num1=8
+num2=2
+print(num1+num2)
+print(num1-num2)
+print(name2*2)
+print(num1/4)
+
+#task 8
+print("*")
+print("**")
+print("***")
+print("****")
+
+#task 9 
+name="krish"
+age=43
+isworking=True
+print(name)
+print(age)
+print(isworking)
+name="Ram"
+age+=1
+isworking=not isworking
+print(name)
+print(age)
+print(isworking)
+
+#task 10
+name="Ram"
+age=26
+dob="20-05-1999"
+hgt=175.5
+wgt=65.4
+deg="B.E CSE"
+gender="Male"
+print("Name of the employee is ",name)
+print("Age of the employee is ",age)
+print("DOB of the employee",dob)
+print("Height is ",hgt)
+print("Weight is ",wgt)
+print("Degree is ",deg)
+print(gender)
